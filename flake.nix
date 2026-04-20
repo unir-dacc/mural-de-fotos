1f10d8021209e7ee0c0b6db11b3df27f885dac2e
Ricardo Alves <ricardcpu@gmail.com>
2026-04-20 14:17:08 -0400

    chore: Add macos support on flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,47 @@
 {
   description = "NixOS environment";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs, }:
+  outputs =
+    { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShell.${system} = with pkgs;
-        mkShell {
-          packages = [
-            nodejs_22
-            yarn
-            nodePackages.typescript
-            nodePackages.typescript-language-server
-            openssl
-            pkg-config
-            pre-commit
-          ];
-        };
+      # Define os sistemas suportados
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      # Função auxiliar para gerar os outputs para cada sistema
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Instancia o nixpkgs para cada sistema
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells = forAllSystems (system: {
+        default =
+          with pkgsFor system;
+          mkShell {
+            packages = [
+              nodejs_22
+              yarn
+              nodePackages.typescript
+              nodePackages.typescript-language-server
+              openssl
+              pkg-config
+              pre-commit
+            ];
+
+            # Dica: No macOS, algumas ferramentas precisam encontrar o Framework CoreServices
+            buildInputs = lib.optionals stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.CoreServices
+            ];
+          };
+      });
     };
 }
